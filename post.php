3e89ec9c2e41208feacb295b29f25c5180fdb94d
Do-fomin <107024858+Do-fomin@users.noreply.github.com>
2023-06-19 13:53:27 +0300

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
 $_SESSION['username'] = $_POST['username'];
-header("Location: http://localhost/php-homeworks-8/index.php");
+$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://';
+header("Location:" . $protocol . $_SERVER["HTTP_HOST"]);
 exit;
